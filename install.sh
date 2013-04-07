f702b5f1c408a81da338f08e8c0c7a4955818007
Ben <ben@TheBatcave.(none)>
2013-04-07 00:20:03 -0700

    Update

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 sudo cp vimrc ~/.vimrc
-sudo apt-get install build-essential ruby1.8-dev
 sudo apt-get install ctags
-vim command-t-1.4.vba 
-
-
+sudo cp -r ./nerdTree/* ~/.vim
+sudo cp -r ./ctrlp/* ~/.vim
