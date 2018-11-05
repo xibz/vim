f386a0750de68775c4cfede73de1e9ff8c8d5aa3
Baron Von Ben Powell <powellbe@dev-dsk-powellbe-2c-a32d0e4e.us-west-2.amazon.com>
2018-11-05 20:49:21 +0000

    updating to now clone instead of caching the plugins

--- install.sh
+++ install.sh
@@ -22,23 +22,57 @@ echo -e "${GREEN}Installing tmux${NC}"
 sudo apt-get install tmux
 echo -e "${GREEN}Installing ctags${NC}"
 sudo apt-get install ctags
+
+################################
+# Plugins
+################################
 echo -e "${GREEN}Copying nerd tree${NC}"
+git clone git@github.com:scrooloose/nerdtree.git nerdTree
 sudo cp -r ./nerdTree/* ~/.vim
+
 echo -e "${GREEN}Copying ctrlp${NC}"
+git clone git@github.com:kien/ctrlp.vim.git ctrlp
 sudo cp -r ./ctrlp/* ~/.vim
+
+echo -e "${GREEN}Copying pathogen${NC}"
+git clone git@github.com:tpope/vim-pathogen.git pathogen
+sudo cp -r ./pathogen/* ~/.vim
+
+echo -e "${GREEN}Copying tagbar${NC}"
+git clone git@github.com:majutsushi/tagbar.git tagbar
+sudo cp -r ./tagbar/* ~/.vim
+
+################################
+# Syntax
+################################
 echo -e "${GREEN}Copying ftdetect${NC}"
 sudo cp -rf ./ftdetect/ ~/.vim
 echo -e "${GREEN}Copying ftplugin${NC}"
 sudo cp -rf ./ftplugin/ ~/.vim
 echo -e "${GREEN}Copying syntax${NC}"
 sudo cp -rf ./syntax/ ~/.vim
-mkdir -p ~/.vim/bundle
+
+################################
+# Bundles
+################################
+# delete the current bundle folder in
+# the vim directory
+if [ -d ~/.vim/bundle]; then
+rm -rf ~/.vim/bundle
+fi
+
+mkdir bundle
+cd bundle
 echo -e "${GREEN}Installing Vundle${NC}"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git
 echo -e "${GREEN}Installing easy motion${NC}"
-git clone https://github.com/Lokaltog/vim-easymotion.git ~/.vim/bundle/vim-easymotion
+git clone https://github.com/Lokaltog/vim-easymotion.git
 echo -e "${GREEN}Installing vim go${NC}"
-git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
+git clone https://github.com/fatih/vim-go.git
+
+echo -e "${GREEN}Symlinking bundle folder${NC}"
+ln -s ~/vim/bundle ~/.vim/bundle
+
 echo -e "${GREEN}Exporting variables to .bash_profile${NC}"
 echo "export PATH=$PATH:/$HOME/Go/bin" >> $HOME/.bash_profile 
 mkdir $HOME/Go
